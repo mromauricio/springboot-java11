ad25a42de9ecaee2dcdc275a0c4a27ede9ca02b2
Mauricio <mromauricio@gmail.com>
2022-02-05 16:24:55 -0300

    User delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -25,4 +25,11 @@ public class UserService {
 		return obj.get();
 	}
 
+	public User insert(User obj){
+		return repository.save(obj);
+	}
+
+	public void delete(Long id){
+		repository.deleteById(id);
+	}
 }
